267a8f9aa3fb07f1ea2542d815041c8ea21a8e6b
Ian Brash <brash@usc.edu>
2021-07-27 13:15:06 -0500

    Continue on Walmart flow

--- src/Logic/sites/Walmart/flow/checkout.ts
+++ src/Logic/sites/Walmart/flow/checkout.ts
@@ -5,11 +5,13 @@ import {
     accumulateCookies
 } from '../../../requestFunctions'
 import axios from 'axios';
+import timestampLogger from '../../../logger';
 
 const flow = async () => {
     let productUrl = 'https://www.walmart.com/ip/Starbucks-Dark-Roast-Whole-Bean-Coffee-Sumatra-100-Arabica-1-bag-12-oz/20709874'
     let allCookies : string[] = [];
 
+    timestampLogger("Getting the product")
     let GETProductResponse = await axios({
         method: 'get',
         url: productUrl,
@@ -33,8 +35,6 @@ const flow = async () => {
 
     allCookies = accumulateCookies(allCookies, returnParsedCookies(GETProductResponse.headers['set-cookie']) )
 
-    console.log(allCookies)
-
     var data = JSON.stringify({
         "offerId": "3D437062270B4972BFB09E2B74794114",
         "quantity": 1,
@@ -54,6 +54,9 @@ const flow = async () => {
         ]
     });
 
+
+
+    timestampLogger("Adding to cart")
     // 400 = offerId is invalid
     const ATCResponse = await axios({
         method: 'post',
@@ -79,7 +82,6 @@ const flow = async () => {
 
 
     allCookies = accumulateCookies(allCookies, returnParsedCookies(ATCResponse.headers['set-cookie']) )
-    console.log(allCookies)
 
     var GETCheckoutResponseData = JSON.stringify({
         "storeList": [
@@ -124,6 +126,9 @@ const flow = async () => {
         "affiliateInfo:com.wm.reflector": ""
       });
 
+
+
+    timestampLogger("Getting Checkout")
     const GETCheckoutResponse = await axios({
         method: 'post',
         url: 'https://www.walmart.com/api/checkout/v3/contract?page=CHECKOUT_VIEW',
@@ -148,10 +153,96 @@ const flow = async () => {
             cookie: joinCookies(allCookies)
         },
         data : GETCheckoutResponseData,
-        validateStatus: () => true
     })
 
-    console.log(GETCheckoutResponse)
+    // https://www.walmart.com/px/PXu6b0qd2S/captcha/captcha.js?a=c&m=0&u=79b804b0-ef05-11eb-87c9-37b2ea7c0201__79b804b0-ef05-11eb-87c9-37b2ea7c0201&v=&g=b
+
+    // console.log(GETCheckoutResponse)
+
+    allCookies = accumulateCookies(allCookies, returnParsedCookies(GETCheckoutResponse.headers['set-cookie']))
+
+    const ContinueAsGuestData = JSON.stringify({
+        "responseGroup": "BASICPLUS",
+        "includePickUpLocation": true,
+        "clientName": "Web-Checkout-FetchContract",
+        "storeMeta": false,
+        "plus": true
+    });
+
+    timestampLogger("Continuing as guest")
+    const ContinueAsGuestResponse = await axios({
+        method: 'post',
+        url: 'https://www.walmart.com/account/api/location',
+        headers: {
+            'authority': 'www.walmart.com', 
+            'pragma': 'no-cache', 
+            'cache-control': 'no-cache', 
+            'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"', 
+            'accept': 'application/json', 
+            'dnt': '1', 
+            'sec-ch-ua-mobile': '?0', 
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+            'content-type': 'application/json', 
+            'origin': 'https://www.walmart.com', 
+            'sec-fetch-site': 'same-origin', 
+            'sec-fetch-mode': 'cors', 
+            'sec-fetch-dest': 'empty', 
+            'referer': 'https://www.walmart.com/checkout/', 
+            'accept-language': 'en-US,en;q=0.9', 
+            cookie: joinCookies(allCookies)
+        },
+        data : ContinueAsGuestData
+    })
+
+    allCookies = accumulateCookies(allCookies, returnParsedCookies(ContinueAsGuestResponse.headers['set-cookie']))
+
+
+    var SelectDeliveryMethodResponseData = JSON.stringify({
+        "groups": [
+          {
+              // "Ship to house" this probably doesn't change
+            "fulfillmentOption": "S2H",
+            "itemIds": [
+              "fc3280fe-af0a-4e3b-9fdf-b89935d3359f"
+            ],
+            // this comes from the loaded page
+            // it has like 'automation-id="shipMethod-RUSH"'
+            "shipMethod": "RUSH"
+          }
+        ]
+      });
+
+      timestampLogger("Selecting delivery method")
+      const SelectDeliveryMethodResponse = await axios({
+        method: 'post',
+        url: 'https://www.walmart.com/api/checkout/v3/contract/:PCID/fulfillment',
+        headers: {
+            'authority': 'www.walmart.com', 
+            'pragma': 'no-cache', 
+            'cache-control': 'no-cache', 
+            'accept': 'application/json, text/javascript, */*; q=0.01', 
+            'dnt': '1', 
+            'inkiru_precedence': 'false', 
+            'wm_cvv_in_session': 'true', 
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36', 
+            'wm_vertical_id': '0', 
+            'content-type': 'application/json', 
+            'origin': 'https://www.walmart.com', 
+            'sec-fetch-site': 'same-origin', 
+            'sec-fetch-mode': 'cors', 
+            'sec-fetch-dest': 'empty', 
+            'referer': 'https://www.walmart.com/checkout/', 
+            'accept-language': 'en-US,en;q=0.9', 
+            cookie: joinCookies(allCookies)
+        },
+        data : SelectDeliveryMethodResponseData,
+        validateStatus: () => true
+    })
+    allCookies = accumulateCookies(allCookies, returnParsedCookies(SelectDeliveryMethodResponse.headers['set-cookie']))
+
+    console.log(SelectDeliveryMethodResponse)
+    
+
 };
 
 
